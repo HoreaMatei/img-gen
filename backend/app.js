fc61508c0b54fd3f494e28a94dcf356fce86ba1c
HoreaMatei <mateihorea17@yahoo.com>
2025-02-09 17:17:00 +0200

    cors3

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 const allowedOrigins = [
+  "https://img-gen-three.vercel.app",
   "https://img-gen-njso.vercel.app",
   "http://localhost:5173",
 ];
