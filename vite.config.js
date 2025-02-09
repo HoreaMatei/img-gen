48e5b5d3df16069f810ddf96eb02a9d65d0c7ea6
HoreaMatei <mateihorea17@yahoo.com>
2025-02-09 17:36:07 +0200

    final commit

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // When you call /api/login from your frontend, it will be proxied to your backend.
-      "/api": {
-        target: "https://img-gen-three.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
